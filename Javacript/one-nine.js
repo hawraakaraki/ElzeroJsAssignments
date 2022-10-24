aefb145a6be63ce14294b687c64f037e0f6b9b7a
hawraakaraki <hawraakaraki@gmail.com>
2022-10-24 14:26:57 +0300

    update

---
+++ Javacript/one-nine.js
@@ -0,0 +1,33 @@
+// JavaScript Bootcamp assignments from Lesson 001 to 009 
+
+// first assignments
+/* 
+first code not work becouse is before the h1 tag
+secound code work
+third code work
+*/
+// secound assignments 
+document.write ("<h1> Elzero </h1>");
+document.querySelector("h1").style.color="blue";
+document.querySelector("h1").style.fontSize="80px";
+document.querySelector("h1").style.fontFamily="Arial";
+document.querySelector("h1").style.textAlign="center";
+document.querySelector("h1").style.fontWeight="bold";
+// Third assignments 
+console.log (" %cElzero %cWeb %cSchool",
+ "color : red ; font-size : 40px; " ,
+ "color :  green ; font-size : 40px; font-weight: bold ;",
+ "color : blue ; font-size : 40px; " , )
+// fourth assignments 
+console.group("Group 1")
+console.log("Message one");
+console.log("Message two");
+console.group("Child Group")
+console.log("Message one");
+console.log("Message two");
+console.group("Grand Child Group")
+console.log("Message one");
+console.log("Message two");
+console.group("Group 2")
+console.log("Message one");
+console.log("Message two");
