ef4fb14752bfb3f0ee21fef8ff0b4d8ec50165d5
hawraakaraki <hawraakaraki@gmail.com>
2022-12-13 13:44:56 +0200

    update

---
+++ Javascript/learn.js
@@ -0,0 +1,8 @@
+let theAge = 90;
+theAge < 20
+    ? console.log(20) 
+    : theAge > 20 && theAge < 60
+ ? console.log("20 to 60")
+ : theAge > 60
+  ? console.log("Larger Than 60")
+  : console.log("Unknown");
