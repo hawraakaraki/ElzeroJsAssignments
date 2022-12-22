458b1235c1b52ebcd33e0b878d8e97f2fbf562f9
hawraakaraki <hawraakaraki@gmail.com>
2022-12-22 15:50:30 +0200

    update

---
+++ Javascript/54-56.js
@@ -0,0 +1,14 @@
+//1
+
+ friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+ index = 0;
+let counter = 0;
+
+while(counter<friends.length){
+ if(typeof friends[counter]!=="number" && friends[counter].charAt(0)!=="A"){
+         
+               console.log(`${++index}==>${friends[counter]}`)
+          
+     }
+     counter++
+}
